eed0c7a2d08da0e6269459734f3af3597666972c
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-01 18:48:52 +0100

    structure

--- day1.py
+++ day1.py
@@ -1,17 +1,36 @@
+# Task: https://adventofcode.com/2021/day/1
+#Load lines
 file = open('day1_data.txt', 'r')
 Lines = file.readlines()
 file.close()
-sliding_window_size = 3
-count = 0
-last_window = []
-current_window = []
 
-for i in range(0,int(len(Lines)-(sliding_window_size))):
-    current_window = [int(Lines[i]),int(Lines[i+1]),int(Lines[i+2])]
 
-    if(sum(current_window) > sum(last_window)):
-        count+=1
+def part1():
+    count = 0
+    last_line = ""
 
-    last_window = current_window
-    
-print("count:",count)
+    for line in Lines:
+        current_line = int(line.strip())
+        if last_line != "":
+            if last_line < current_line:
+                count+=1
+        last_line = current_line
+
+    return count # 1715
+
+
+def part2():
+    count = 0
+    window_size = 3
+    last_window = []
+    current_window = []
+
+    for i in range( 0, len(Lines)-(window_size) ): # range(0,2000-3)
+        current_window = [int(Lines[i]),int(Lines[i+1]),int(Lines[i+2])]
+        if(sum(current_window) > sum(last_window)):
+            count+=1
+
+        last_window = current_window
+
+    return count # 1739
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import day1 as d1
+
+if __name__ == "__main__":
+    print(d1.part1())
+    print(d1.part2())
