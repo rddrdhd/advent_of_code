c620f147c24f751071dedf29d34527bc4c7b128e
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-01 16:28:48 +0100

    Day 1, part 1

---
+++ day1.py
@@ -0,0 +1,20 @@
+# Using readlines()
+file1 = open('day1_data.txt', 'r')
+Lines = file1.readlines()
+ 
+count = 0
+last_line = ""
+
+for line in Lines:
+    current_line = line.strip()
+    if last_line != "":
+        if int(last_line) < int(current_line):
+            count+=1
+            #print("{} < {} (Increased)".format(last_line, current_line))
+        else:
+            print("{} >= {} (Decreased)".format(last_line, current_line))
+    else:
+        print("no last line")
+    last_line = current_line
+
+print(count) # 1713: your answer is too low.
