d42790d198750cfce8aaa2541ea50568d2172f2d
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-05 11:40:06 +0100

    y2021 d5 p2

--- y2021/day5.py
+++ y2021/day5.py
@@ -73,14 +73,10 @@ def part2():
         numbers = line.replace(",", " ").replace(" -> ", " ")
 
         x1, y1, x2, y2 = map(int, numbers.split(" "))
-        if x1 > max_x:
-            max_x = x1
-        if y1 > max_y:
-            max_y = y1
-        if x2 > max_x:
-            max_x = x2
-        if y2 > max_y:
-            max_y = y2
+
+        max_x = max([x1, x2, max_x])
+        max_y = max([y1, y2, max_y])
+
         values.append([x1, y1, x2, y2])
 
     # fill 2D diagram with zeros
@@ -89,21 +85,22 @@ def part2():
 
     # fill the diagram with values
     for value in values:  # for line
-        bigger_y = max(value[Y1], value[Y2])
-        smaller_y = min(value[Y1], value[Y2])
-        bigger_x = max(value[X1], value[X2])
-        smaller_x = min(value[X1], value[X2])
 
         # add horizontal and vertical stuff
         if value[X1] == value[X2]:
+            bigger_y = max(value[Y1], value[Y2])
+            smaller_y = min(value[Y1], value[Y2])
             for y in range(smaller_y, bigger_y + 1):
                 diagram[y][value[X1]] += 1
         if value[Y1] == value[Y2]:
+            bigger_x = max(value[X1], value[X2])
+            smaller_x = min(value[X1], value[X2])
             for x in range(smaller_x, bigger_x + 1):
                 diagram[value[Y1]][x] += 1
 
         # add diagonal stuff
-        if bigger_x - bigger_y == smaller_x - smaller_y:  #
+        if abs(value[X1]-value[X2]) == abs(value[Y1]-value[Y2]):
+
             # make all coordinates left->right
             if value[X1] > value[X2]:
                 value[X1], value[X2] = value[X2], value[X1]
