1cd9d95c49d5a49ee4ad7d9c35be2b9466f04a55
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-05 11:17:00 +0100

    y2021 d5 p2

--- y2021/day5.py
+++ y2021/day5.py
@@ -4,22 +4,27 @@ f = open('y2021/data/day5.txt', 'r')
 lines = f.readlines()
 f.close()
 
+# array indexes for coordinates
 X1 = 0
 Y1 = 1
 X2 = 2
 Y2 = 3
 
+
 def part1():
+    # for diagram
     max_x = 0
     max_y = 0
     diagram = []
+
+    # for coordinates
     values = []
 
-    # parse data into values
-    for l, line in enumerate(lines):
-        numbers = line.replace(","," ").replace(" -> "," ")
+    # parse lines into values
+    for line in lines:
+        numbers = line.replace(",", " ").replace(" -> ", " ")
 
-        x1,y1,x2,y2 = map(int,numbers.split(" "))
+        x1, y1, x2, y2 = map(int, numbers.split(" "))
         if x1 > max_x:
             max_x = x1
         if y1 > max_y:
@@ -28,25 +33,91 @@ def part1():
             max_x = x2
         if y2 > max_y:
             max_y = y2
-        values.append([x1,y1,x2,y2])
+        values.append([x1, y1, x2, y2])
 
     # fill 2D diagram with zeros
-    for y in range(max_y+1):
-        diagram.append([0 for _ in range(max_x+1)])
+    for y in range(max_y + 1):
+        diagram.append([0 for _ in range(max_x + 1)])
 
     # fill the diagram with values
     for value in values:  # for line
-        print(value[X1], value[Y1], value[X2], value[Y2])
-        if value[X1]==value[X2]:
+        if value[X1] == value[X2]:
             bigger_y = max(value[Y1], value[Y2])
             smaller_y = min(value[Y1], value[Y2])
-            for y in range(smaller_y, bigger_y+1):
+            for y in range(smaller_y, bigger_y + 1):
                 diagram[y][value[X1]] += 1
-        if value[Y1]==value[Y2]:
+        if value[Y1] == value[Y2]:
             bigger_x = max(value[X1], value[X2])
             smaller_x = min(value[X1], value[X2])
-            for x in range(smaller_x, bigger_x+1):
+            for x in range(smaller_x, bigger_x + 1):
                 diagram[value[Y1]][x] += 1
+
+    # sum the overlapping area
+    sum = 0
+    for line in diagram:
+        for num in line:
+            if num > 1:
+                sum += 1
+
+    return sum  # 7380
+
+
+def part2():
+    max_x = 0
+    max_y = 0
+    diagram = []
+    values = []
+
+    # parse data into values
+    for line in lines:
+        numbers = line.replace(",", " ").replace(" -> ", " ")
+
+        x1, y1, x2, y2 = map(int, numbers.split(" "))
+        if x1 > max_x:
+            max_x = x1
+        if y1 > max_y:
+            max_y = y1
+        if x2 > max_x:
+            max_x = x2
+        if y2 > max_y:
+            max_y = y2
+        values.append([x1, y1, x2, y2])
+
+    # fill 2D diagram with zeros
+    for y in range(max_y + 1):
+        diagram.append([0 for _ in range(max_x + 1)])
+
+    # fill the diagram with values
+    for value in values:  # for line
+        bigger_y = max(value[Y1], value[Y2])
+        smaller_y = min(value[Y1], value[Y2])
+        bigger_x = max(value[X1], value[X2])
+        smaller_x = min(value[X1], value[X2])
+
+        # add horizontal and vertical stuff
+        if value[X1] == value[X2]:
+            for y in range(smaller_y, bigger_y + 1):
+                diagram[y][value[X1]] += 1
+        if value[Y1] == value[Y2]:
+            for x in range(smaller_x, bigger_x + 1):
+                diagram[value[Y1]][x] += 1
+
+        # add diagonal stuff
+        if bigger_x - bigger_y == smaller_x - smaller_y:  #
+            # make all coordinates left->right
+            if value[X1] > value[X2]:
+                value[X1], value[X2] = value[X2], value[X1]
+                value[Y1], value[Y2] = value[Y2], value[Y1]
+
+            diagonal_length = value[X2] - value[X1]
+            distance_range = range(abs(diagonal_length) + 1)
+
+            if value[Y1] < value[Y2]:  # diagonal going down
+                for d in distance_range:
+                    diagram[value[Y1] + d][value[X1] + d] += 1
+            else:  # diagonal going up
+                for d in distance_range:
+                    diagram[value[Y1] - d][value[X1] + d] += 1
 
     # sum the overlapping
     sum = 0
@@ -54,11 +125,5 @@ def part1():
         for num in line:
             if num > 1:
                 sum += 1
-        print(line)
 
-    return sum
-
-
-def part2():
-    count = 0
-    return count
+    return sum  # 21373
