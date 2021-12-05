86d1e86e303d1966005d676197c7b59dd7e65833
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-05 13:21:28 +0100

    y2021 d3 p2

--- y2021/day3.py
+++ y2021/day3.py
@@ -1,30 +1,72 @@
 # https://adventofcode.com/2021/day/3
 
 f = open('y2021/data/day3.txt', 'r')
+#f = open('y2021/data/test.txt', 'r')
 lines = f.readlines()
 f.close()
 
 
-def part1():
-    counter = [0 for _ in range(len(lines[0].strip()))]
-    gamma_rate = "0b"
-    consumption_rate = "0b"
-
+def is_zero_most_common(lines, position, or_equal=True):
+    count = 0
     for line in lines:
-        for position in range(len(line.strip())):
-            counter[position] += int(line[position])
+        if line[position] == "0":
+            count += 1
+    if count < (len(lines)-count):
+        return True
+    elif count == (len(lines)-count):
+        return or_equal
+    return False
 
-    for position_count in counter:
-        if position_count > int(len(lines) / 2):  # 1 is most used on this position
-            gamma_rate += "1"
-            consumption_rate += "0"
-        else:
-            gamma_rate += "0"
-            consumption_rate += "1"
 
+def part1():
+    row_length = len(lines[0].strip())
+    gamma_rate = ""
+    consumption_rate = ""
+
+    for column in range(row_length):
+        gamma_rate += "0" if is_zero_most_common(lines,column,False) else "1"
+        consumption_rate += "1" if is_zero_most_common(lines,column,True) else "0"
     return int(gamma_rate, 2) * int(consumption_rate, 2)
 
-
 def part2():
-    count = 0
-    return count
+    oxygen = lines
+    oxygen_rating = 0
+
+    for i in range(len(lines[0].strip())):
+        bit_counter = {'0': 0, '1': 0}
+
+        for number in oxygen:
+            bit_counter[number[i]] += 1
+
+        if bit_counter['1'] >= bit_counter['0']:
+            most_common = '1'
+
+        else:
+            most_common = '0'
+
+        oxygen = [n for n in oxygen if n[i] == most_common]
+
+        if len(oxygen) == 1:
+            oxygen_rating = int(oxygen[0], 2)
+
+    co2 = lines
+    co2_rating = 0
+
+    for i in range(len(lines[0].strip())):
+        bit_counter = {'0': 0, '1': 0}
+
+        for number in co2:
+            bit_counter[number[i]] += 1
+
+        if bit_counter['1'] < bit_counter['0']:
+            least_common = '1'
+
+        else:
+            least_common = '0'
+
+        co2 = [n for n in co2 if n[i] == least_common]
+
+        if len(co2) == 1:
+            co2_rating = int(co2[0], 2)
+    return int(oxygen_rating)*int(co2_rating) #  3765399
+
