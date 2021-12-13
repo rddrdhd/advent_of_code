7f327daf11cf94c3dc10df3cc326c04b6c12df79
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-13 22:15:37 +0100

    y2021 d12

--- y2021/day12.py
+++ y2021/day12.py
@@ -1,12 +1,42 @@
-# Task: https://adventofcode.com/2021/day/X
-f = open('y2021/data/test.txt', 'r')
+# Task: https://adventofcode.com/2021/day/12
+# https://www.geeksforgeeks.org/generate-graph-using-dictionary-python/
+from collections import defaultdict
+
+f = open('y2021/data/day12.txt', 'r')
 lines = f.readlines()
 f.close()
+lines = [s.strip() for s in lines]
+
+graph = defaultdict(list)
+
+for line in lines:
+    node1, node2 = line.strip().split("-")
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+
+
+def get_all_paths(graph, start, end, path=[], repeats=False):
+    path = path + [start]
+    if start == end:
+        return [path]
+    paths = []
+
+    for node in graph[start]:
+        new_paths = []
+        if node.isupper() or node not in path:
+            new_paths += get_all_paths(graph, node, end, path, repeats)
+        elif node.islower() and node in path and repeats and node not in ('start', 'end'):
+            new_paths += get_all_paths(graph, node, end, path, False)
+        for new_path in new_paths:
+            paths.append(new_path)
+    return paths
+
 
 def part1():
-    for line in lines:
-        print(line)
-    return 0
+    paths = get_all_paths(graph, "start", "end", repeats=False)
+    return len(paths)  # 4241
+
 
 def part2():
-    return 0
+    paths = get_all_paths(graph, "start", "end", repeats=True)
+    return len(paths)  # 122134
