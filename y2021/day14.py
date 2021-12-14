bca29f64d491056697e4fec727067dd6c8dbc989
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2021-12-14 13:26:22 +0100

    y2021 d14 p2

--- y2021/day14.py
+++ y2021/day14.py
@@ -1,48 +1,60 @@
-# Task: https://adventofcode.com/2021/day/X
-from collections import Counter
+# Task: https://adventofcode.com/2021/day/14
+from collections import defaultdict
 
-f = open('y2021/data/test.txt', 'r')
+f = open('y2021/data/day14.txt', 'r')
 lines = f.readlines()
 f.close()
-
 lines = [s.strip() for s in lines]
 
-def make_steps(steps, rules, template):
-    done = []
-    todo = [x for x in template]
-    for step in range(steps):
-        while len(todo) - 1:
-            first = todo.pop(0)
-            second = todo[0]
-            if len(done) == 0:
-                done.append(first)
 
-            if str(first + second) in rules:
-                done.append(rules[str(first + second)])
-                done.append(second)
-
-        if step+1 == steps:
-            return done
-        todo = done.copy()
-        done = []
-
-def part1(steps=10):
+def get_data():
     template, _ = lines[:2]
     insertion_rules = lines[2:]
     rules = {}
     for line in insertion_rules:
         key, value = line.strip().split(" -> ")
         rules[key] = value
-
-    done = make_steps(steps, rules, template)
-    x = Counter(done)
-
-    max_key = max(x, key=x.get)
-    min_key = min(x, key=x.get)
-
-    return x[max_key]-x[min_key]
+    return template, rules
 
 
-def part2():
-    # TODO only with counting
-    return 0
+def make_steps(steps, rules, template):
+    pairs_counter = defaultdict(int)
+    for i in range(1, len(template)):
+        pairs_counter[str(template[i - 1]) + str(template[i])] += 1
+
+    # get pairs in final string
+    for _ in range(steps):
+        new_pairs = defaultdict(int)
+        for pair in pairs_counter:
+            for i in rules[pair]:
+                new_pairs[pair[0] + i] += pairs_counter[pair]
+                new_pairs[i + pair[1]] += pairs_counter[pair]
+        pairs_counter = new_pairs
+
+    # get result
+    char_counter = defaultdict(int)
+    for p in pairs_counter:
+        for char in list(p):
+            char_count = 0
+            for pair, count in pairs_counter.items():
+                if char == pair[0]:
+                    char_count += count
+            char_counter[char] = char_count
+
+    # last char
+    char_counter[template[-1]] += 1
+
+    # return
+    max_key = max(char_counter, key=char_counter.get)
+    min_key = min(char_counter, key=char_counter.get)
+    return char_counter[max_key] - char_counter[min_key]
+
+
+def part1(steps=10):
+    template, rules = get_data()
+    return make_steps(steps, rules, template)  # 2899
+
+
+def part2(steps=40):
+    template, rules = get_data()
+    return make_steps(steps, rules, template)  # 3528317079545
