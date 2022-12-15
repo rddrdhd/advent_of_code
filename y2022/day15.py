68cffe5e5bf9f98967bccc268709f312412be517
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2022-12-15 14:32:26 +0100

    WIP: day15p1

---
+++ y2022/day15.py
@@ -0,0 +1,83 @@
+import re
+
+f = open('y2022/data/day15.txt', 'r')
+lines = f.readlines()
+f.close()
+lines = [l.strip() for l in lines]
+
+
+class Map:
+    sensors = []
+    beacons = []
+    empty = []
+    reaches = []
+    min_x = 0
+    max_x = 0
+    min_y = 0
+    max_y = 0
+
+    def __init__(self, lines):
+        for line in lines:
+            sx, sy, bx, by = [int(j) for j in re.findall(r'\d+', line)]
+            self.sensors.append([int(sy), int(sx)])
+            self.beacons.append([int(by), int(bx)])
+            self.reaches.append(int(abs(sy - by) + abs(sx - bx)))
+        self.min_x = min(i[1] for i in self.beacons + self.sensors) - max(self.reaches)
+        self.max_x = max(i[1] for i in self.beacons + self.sensors) + max(self.reaches)
+        self.min_y = min(i[0] for i in self.beacons + self.sensors) - max(self.reaches)
+        self.max_y = max(i[0] for i in self.beacons + self.sensors) + max(self.reaches)
+
+    def fill_empty(self):
+        for i in range(len(self.sensors)):
+            print(self.reaches)
+            print(self.reaches[i])
+
+            for y in range(0, self.reaches[i] + 1):
+                for x in range(0, self.reaches[i] + 1):
+                    if x + y <= self.reaches[i]:
+                        a = [self.sensors[i][0] + y, self.sensors[i][1] + x]
+                        b = [self.sensors[i][0] - y, self.sensors[i][1] - x]
+                        c = [self.sensors[i][0] + y, self.sensors[i][1] - x]
+                        d = [self.sensors[i][0] - y, self.sensors[i][1] + x]
+                        for e in a, b, c, d:
+                            if e not in self.empty:
+                                self.empty.append(e)
+
+    def print(self):
+        print(self.sensors)
+        print(self.beacons)
+
+        for y in range(self.min_y, self.max_y):
+            print(y, end="\t")
+            for x in range(self.min_x, self.max_x):
+                if [y, x] in self.beacons:
+                    ch = "B"
+                elif [y, x] in self.sensors:
+                    ch = "S"
+                elif [y, x] in self.empty:
+                    ch = "#"
+                else:
+                    ch = "."
+                print(ch, end="")
+            print()
+
+    def get_not_beacon_spots(self, row):
+        count = 0
+        for e in self.empty:
+            if e[0] == row and e not in self.beacons and e not in self.sensors:
+                count += 1
+                print(e)
+        return count
+
+
+def part1():
+    #TODO: make it... not O(x^x^x^x^x^x^x^x...)
+    m = Map(lines)
+    m.fill_empty()
+    m.print()
+    return m.get_not_beacon_spots(10)
+    # return m.get_not_beacon_spots(2000000)
+
+
+def part2():
+    return 0
