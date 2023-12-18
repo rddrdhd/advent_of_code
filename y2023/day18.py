535c25d91af808cdc057e6b49d9a558886816ba5
Leona Zurkova <zur0037@vsb.cz>
2023-12-18 21:07:48 +0100

    y2023d18

--- y2023/day18.py
+++ y2023/day18.py
@@ -16,55 +16,39 @@ UP = 'U'
 
 NEIGHTBOURS = {RIGHT:(0,1), DOWN:(1, 0), LEFT:(0,-1), UP:(-1, 0)}
 
-def part1():
+def get_full_area(border, corners):
+    crosses = 0
+    for i in range(len(corners)):
+        curr_x= corners[i][X]
+        prev_y = corners[i - 1][Y]
+        next_y = corners[(i + 1) % len(corners)][Y]
+        crosses += curr_x * (prev_y - next_y)
+    shoelace = abs(crosses) / 2
+    area = shoelace - border / 2 
+    return int(area + border + 1) 
+
+def get_border_and_corners(lines, second_part=False):
     corners = [(0,0)]
     border = 0
     for line in lines:
-        direction, count, _= line.split(" ")
-        dy, dx = NEIGHTBOURS[direction]
-        count = int(count)
+        direction, count, hex= line.split()
+        hex = hex[2:-1]
+        if not second_part:
+            dy, dx = NEIGHTBOURS[direction]
+            count = int(count)
+        else:
+            direction = [RIGHT,DOWN,LEFT,UP][int(hex[-1])]
+            dy, dx = NEIGHTBOURS[direction]
+            count = int(hex[:-1], 16)
         border += count
         y,x = corners[-1]
-        corners.append((y + dy * count, x + dx * count))
+        corners.append((y+ dy*count, x+dx*count))
+    return border, corners
 
-    crosses = 0
-
-    for i in range(len(corners)):
-        curr_x= corners[i][X]
-        prev_y = corners[i - 1][Y]
-        next_y = corners[(i + 1) % len(corners)][Y]
-        
-        crosses += curr_x * (prev_y - next_y)
-
-    shoelace = abs(crosses) / 2
-    area = shoelace - border / 2 
-
-    return int(area + border + 1) # 36679
+def part1():
+    border, corners = get_border_and_corners(lines, second_part=False)
+    return get_full_area(border, corners) # 36679
     
 def part2():
-    corners = [(0,0)]
-    border = 0
-    for line in lines:
-        hex = line.split(" ")[-1][2:-1]
-        dir_index = int(hex[-1])
-        direction = [RIGHT,DOWN,LEFT,UP][dir_index]
-        dy, dx = NEIGHTBOURS[direction]
-        count_hex = hex[:-1]
-        count = int(count_hex, 16)
-        border += count
-        y, x = corners[-1]
-        corners.append((y + dy * count, x + dx * count))
-
-    crosses = 0
-
-    for i in range(len(corners)):
-        curr_x= corners[i][X]
-        prev_y = corners[i - 1][Y]
-        next_y = corners[(i + 1) % len(corners)][Y]
-        
-        crosses += curr_x * (prev_y - next_y)
-
-    shoelace = abs(crosses) / 2
-    area = shoelace - border / 2 
-
-    return int(area + border + 1) # 88007104020978
+    border, corners = get_border_and_corners(lines, second_part=True)
+    return get_full_area(border, corners) # 88007104020978
