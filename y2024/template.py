844979d7f44825c94df3220b80d1c903ef424eb9
rddrdhd <zurkova.leona@gmail.com>
2024-12-02 16:56:11 +0100

    template

---
+++ y2024/template.py
@@ -0,0 +1,21 @@
+#Task: https://adventofcode.com/2024/day/1
+f = open('y2024/data/day01.txt', 'r')
+lines = f.readlines()
+f.close()
+
+lines = [s.strip() for s in lines]
+
+
+def part1():
+    sum = 0
+    return sum
+
+
+def part2():
+    sum = 0
+    return sum
+
+if __name__ == "__main__":
+    print()
+    print("P1",part1())
+    print("P2",part2())
